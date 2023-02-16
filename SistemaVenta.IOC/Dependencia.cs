44bae2852b3065184ffcc2d8cbe2df34c7287a3b
jorcartriyo <33526483+jorcartriyo@users.noreply.github.com>
2023-02-16 10:48:12 +0100

    Add files via upload

---
+++ SistemaVenta.IOC/Dependencia.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using SistemaVenta.DAL.DBContext;
+using SistemaVenta.DAL.Interfaces;
+using SistemaVenta.DAL.Implementation;
+using SistemaVenta.BLL.Interfaces;
+using SistemaVenta.BLL.Implementation;
+
+namespace SistemaVenta.IOC
+{
+    public static class Dependencia
+    {
+        public static void InyectarDependencia(this IServiceCollection services, IConfiguration Configuration)
+        {
+            services.AddDbContext<DbventaContext>(options =>
+                options.UseMySQL(Configuration.GetConnectionString("DevConnection"))
+            );
+            services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+            services.AddScoped<IVentaRepository, VentaRepository>();
+            services.AddScoped<ICorreoService, CorreoService>();
+            services.AddScoped<IFireBaseService, FireBaseService>();
+            services.AddScoped<IUtilidadesService, UtilidadesService>();
+            services.AddScoped<IRolService, RolService>();
+        }
+    }
+}
